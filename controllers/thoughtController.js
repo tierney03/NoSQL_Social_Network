6a6572d1208d9c510cbc862ff8080b188dfe55bf
tierney03 <tierney_03@mac.com>
2022-12-08 08:43:46 -0600

    Working on thought Controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,4 +1,70 @@
 const { ObjectId } = require("mongoose").Types;
+const { count } = require("console");
 const { User, Thought } = require("../models");
 
-module.exports = {};
+module.exports = {
+  getThoughts(req, res) {
+    Thought.find()
+      .then(async (thoughts) => {
+        const thoughtObj = {
+          thoughts,
+          count: await count(),
+        };
+        return res.json(thoughtObj);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.studentId })
+      .select("-__v")
+      .then(async (thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought with that ID" })
+          : res.json({
+              thought,
+              thoughts: await thoughts(req.params.thoughtId),
+            })
+      )
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((thought) => res.jason(thought))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  deleteThought(req, res) {
+    Thought.findOneAndRemove({ _id: req.params.thoughtId })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No such thought exists" })
+          : User.findOneAndUpdate(
+              { thoughts: req.params.thoughtId },
+              { $pull: { thoughts: req.params.thoughtId } },
+              { new: true }
+            )
+      )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({
+              message: "Thought deleted, but no users found",
+            })
+          : res.json({ message: "Thought successfully deleted" })
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  addReaction,
+  removeReaction,
+};
