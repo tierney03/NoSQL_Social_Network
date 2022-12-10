14934a8084884c9c7b919af5424686b72ed2a2e3
tierney03 <tierney_03@mac.com>
2022-12-10 13:37:40 -0600

    correcting message error

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const apiRoutes = require("./api");
 router.use("/api", apiRoutes);
 
 router.use((req, res) => {
-  res.status(404).send(message, "Error");
+  res.status(404).send("Not working");
 });
 
 module.exports = router;
